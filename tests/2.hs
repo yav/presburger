ff4f6f5517cc5ffd9689d3a765c2e94e07e30ac5
Iavor S. Diatchki <diatchki@galois.com>
2013-07-11 16:35:16 -0700

    Remove old tests (XXX: port these, eventually)

--- tests/2.hs
+++
@@ -1,8 +0,0 @@
-import Data.Integer.Presburger
-
-mytest1 = Exists $ \i -> Exists $ \j -> i :<: j :/\: j :=: 3
-mytest2 = Exists $ \j -> j :=: 3 :/\: (Exists $ \i -> i :<: j)
-
-main = print (check mytest1 == check mytest2)
-
-
--- tests/5.hs
+++
@@ -1,6 +0,0 @@
-import Data.Integer.Presburger.ModArith
-import Data.Integer.Presburger
-
-main = print $ check $ forall $ \x y z1 z2 ->
-          (add_mod d x y z1 :/\: add_mod d x y z2) :=>: z1 :=: z2
-  where d = 2000
