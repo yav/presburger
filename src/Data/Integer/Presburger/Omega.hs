7dadf535759238a610c4fc2a96386a3366b5f8cd
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2013-10-25 13:36:23 -0700

    Finish up equalities.

--- src/Data/Integer/Presburger/Omega.hs
+++ src/Data/Integer/Presburger/Omega.hs
@@ -8,6 +8,8 @@ import Data.List(sortBy,partition)
 import Control.Applicative
 import Control.Monad
 
+import Debug.Trace
+
 newtype S a = S (RW -> (a,RW))
 
 instance Functor S where
@@ -23,27 +25,43 @@ instance Monad S where
                                S m1   = k a
                            in m1 s1)
 
-updS :: (RW -> RW) -> S ()
-updS f = S (\s -> ((), f s))
+updS_ :: (RW -> RW) -> S ()
+updS_ f = S (\s -> ((), f s))
+
+updS :: (RW -> (a,RW)) -> S a
+updS f = S f
+
+newVar :: S Name
+newVar = updS $ \rw -> (nameSource rw, rw { nameSource = nameSource rw - 1 })
+
+testRun (S m) = m RW { nameSource = -1, inerts = noInerts, todo = [] }
+
 
 
 data Ct = Term :=: Term | Term :<: Term | Ct :\/: Ct
+          deriving Show
 
 
-data RW = RW { nameSource :: Int
+data RW = RW { nameSource :: !Int
              , inerts     :: Inerts
              , todo       :: [Ct]
-             }
+             } deriving Show
 
 data Inerts = Inerts
   { upperBounds :: IntMap (Integer,Term)    -- a |-> (c,t)  <=>  c*a < t
   , lowerBounds :: IntMap (Integer,Term)    -- a |-> (c,t)  <=>  t < c*a
   , solved      :: IntMap Term  -- idempotent subst
-  }
+  } deriving Show
+
+noInerts :: Inerts
+noInerts = Inerts { upperBounds = Map.empty
+                  , lowerBounds = Map.empty
+                  , solved      = Map.empty
+                  }
 
 -- Assumes substitution has already been applied
 addSolved :: Name -> Term -> S ()
-addSolved x t = updS $ \rw ->
+addSolved x t = updS_ $ \rw ->
   let i = inerts rw
       (kickedOutU, otherU) = Map.partitionWithKey kickOut (upperBounds i)
       (kickedOutL, otherL) = Map.partitionWithKey kickOut (lowerBounds i)
@@ -63,38 +81,47 @@ addSolved x t = updS $ \rw ->
   toC f (y,(c,s)) = if x == y then f (c |*| t)      (tLet x t s)
                               else f (c |*| tVar y) (tLet x t s)
   cvt f           = map (toC f) . Map.toList
+
+
 -- Assumes substitution has already been applied
-addEq t1 t2 =
-  let T c xsMap = t1 - t2
-      xs        = Map.toList xsMap
-  in cases c xs
+-- c + xs = 0
+addEq :: Integer -> [(Name,Integer)] -> S ()
 
-  where
-  cases 0 []  = return ()
-  cases _ []  = fail "impossible" -- XXX: do properly
-  cases c [(x,xc)]  = case divMod (-c) xc of
-                        (q,0) -> addSolved x (fromInteger q)
-                        _     -> fail "impossible"
-  cases c xs
-    | ((x,xc) : ys, zs) <- partition ((1 ==) . abs . snd) xs =
-      addSolved x $ let te = T c $ Map.fromList $ ys ++ zs
-                    in if xc > 0 then negate te else te
+addEq 0 []        = return ()
 
-  cases c xs
-    | Just d <- common (c : map snd xs) =
-      addEq (T (div c d) (Map.fromList [ (x, div xc d) | (x,xc) <- xs ])) 0
+addEq _ []        = fail "impossible" -- XXX: do properly
 
+addEq c [(x,xc)]  = case divMod (-c) xc of
+                      (q,0) -> addSolved x (fromInteger q)
+                      _     -> fail "impossible"
 
-{-
--- 2*x + 3*y = 0
-solveEq c xs = undefined
-  where
-  (x,cx) : rest = sortBy (comparing snd) (Map.toList xs)
-  m = abs cx + 1
-  modulus a = a - m * (div (2 * a + m) (2 * m))
--}
+addEq c xs
+  | ((x,xc) : ys, zs) <- partition ((1 ==) . abs . snd) xs =
+    addSolved x $ let te = T c $ Map.fromList $ ys ++ zs
+                  in if xc > 0 then negate te else te
 
+addEq c xs =
+  case common (c : map snd xs) of
+    Just d  -> addEq (div c d) [ (x, div xc d) | (x,xc) <- xs ]
 
+    -- See page 6 of paper
+    Nothing ->
+      do let (x,cx) : rest = sortBy (comparing snd) xs
+             m             = abs cx + 1
+         v <- newVar
+         let sgn  = signum cx
+             soln = sum $ (negate sgn * m) |*| tVar v
+                        : fromInteger (sgn * modulus c m)
+                        : [ (sgn * modulus cy m) |*| tVar y | (y,cy) <- rest ]
+         addSolved x soln
+
+         let upd i = div (2*i + m) (2*m) + modulus i m
+         addEq (upd c) $ (v, negate (abs cx)) : [ (y, upd cy) | (y,cy) <- rest ]
+
+modulus :: Integer -> Integer -> Integer
+modulus a m = a - m * div (2 * a + m) (2 * m)
+
+-- Find a common factor for all the given inputs.
 common :: [Integer] -> Maybe Integer
 common []  = Nothing
 common [x] = Just x
@@ -102,3 +129,6 @@ common (x : y : zs) =
   case gcd x y of
     1 -> Nothing
     n -> common (n : zs)
+
+
+
